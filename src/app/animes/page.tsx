c1e2a4cf70bc960a0450c3edb0268c7b42b39c71
Mazedul Islam <mazidmailbox@gmail.com>
2023-08-12 18:39:45 +0600

    adding animes and movies pages

---
+++ src/app/animes/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AnimesPage() {
+  return <div>AnimesPage</div>;
+}
+
+export default AnimesPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import AuthLinks from "./components/nav/AuthLinks";
 export default async function Home() {
   return (
     <main>
-      <h1>Media Library</h1>
-      <AuthLinks />
+      <h1>Home page</h1>
     </main>
   );
 }
