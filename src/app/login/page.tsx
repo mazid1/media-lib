bf6c55547db20ea348c8c0ac02e2b83c57d86d55
Mazedul Islam <mazidmailbox@gmail.com>
2023-08-12 18:17:34 +0600

    login logout page style updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,74 +23,54 @@ function LoginPage() {
   };
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex gap-4 min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        <h1 className="text-center text-2xl font-bold leading-9 tracking-tight">
           Login to your account
-        </h2>
+        </h1>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" onSubmit={loginUser}>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email address
+          <div className="form-control w-full">
+            <label htmlFor="email" className="label">
+              <span className="label-text">Email address</span>
             </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                value={data.email}
-                onChange={(e) =>
-                  setData((prev) => ({ ...prev, email: e.target.value }))
-                }
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              value={data.email}
+              onChange={(e) =>
+                setData((prev) => ({ ...prev, email: e.target.value }))
+              }
+              className="input input-bordered w-full"
+            />
           </div>
 
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Password
+          <div className="form-control w-full">
+            <label htmlFor="password" className="label">
+              <span className="label-text">Password</span>
             </label>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                value={data.password}
-                onChange={(e) =>
-                  setData((prev) => ({ ...prev, password: e.target.value }))
-                }
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              value={data.password}
+              onChange={(e) =>
+                setData((prev) => ({ ...prev, password: e.target.value }))
+              }
+              className="input input-bordered w-full"
+            />
           </div>
 
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Login
-            </button>
-          </div>
+          <button type="submit" className="btn btn-primary normal-case w-full">
+            Login
+          </button>
         </form>
       </div>
     </div>
