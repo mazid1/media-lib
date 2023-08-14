ed8a01483ff29c2f3a30eeaa458772cdfa01afc0
Mazedul Islam <mazidmailbox@gmail.com>
2023-08-14 07:08:04 +0600

    responsive movie list

--- src/app/components/movies/MovieList.tsx
+++ src/app/components/movies/MovieList.tsx
@@ -9,6 +9,7 @@ import { getPosterUrl } from "@/lib/tmdbImage";
 import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
 import React, { useState } from "react";
 import dayjs from "dayjs";
+import Image from "next/image";
 
 function MovieList() {
   const [page, setPage] = useState(1);
@@ -22,16 +23,21 @@ function MovieList() {
 
   return (
     <div className="flex flex-col">
-      <div className="grid grid-cols-5 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-items-center">
         {data.tmdb.moviePopularList?.results?.map((movie) => (
-          <div key={movie.id} className="card shadow-xl bg-base-200">
+          <div
+            key={movie.id}
+            className="card shadow-xl bg-base-200 max-w-[342px]"
+          >
             <figure>
-              <img
+              <Image
                 src={getPosterUrl({
                   fileSize: "w342",
-                  filePath: movie.posterPath,
+                  filePath: movie.posterPath!,
                 })}
-                alt={movie.title}
+                alt={movie.title!}
+                width={342}
+                height={513}
               />
             </figure>
             <div className="p-4 flex flex-col gap-2">
