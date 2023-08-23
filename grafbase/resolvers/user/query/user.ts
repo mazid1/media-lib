838880624bd097ce768153b1baaa5d345a0e5465
Mazedul Islam <mazidmailbox@gmail.com>
2023-08-23 14:32:25 +0600

    added vercel/postgres

--- grafbase/resolvers/user/query/user.ts
+++ grafbase/resolvers/user/query/user.ts
@@ -1,11 +1,12 @@
 import { QueryUserArgs } from "@/__generated__/types";
+import { db } from "@vercel/postgres";
 
 export default async function User(_: any, { input }: QueryUserArgs) {
-  // const client = await db.connect()
-  console.log(JSON.stringify({ _, input }));
-  return {
-    id: 1,
-    name: "Mazid",
-    email: "mazidmailbox@gmail.com",
-  };
+  const client = await db.connect();
+  const { email } = input;
+  const users =
+    await client.sql`select unique * from "User" u where u.email = ${email}`;
+  console.log(JSON.stringify(users));
+  if (users.rowCount === 0) return null;
+  return users.rows.at(0);
 }
