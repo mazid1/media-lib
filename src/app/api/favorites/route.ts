24efa067b307d76ef240ed0b558a07fe6fe414de
Mazedul Islam <mazidmailbox@gmail.com>
2023-08-27 07:42:03 +0600

    delete favorite

--- src/app/api/favorites/route.ts
+++ src/app/api/favorites/route.ts
@@ -3,16 +3,17 @@ import prisma from "@/lib/prisma";
 import { MediaType, Favorite } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { ErrorBody, SessionUser } from "@/lib/types";
-
-type FavoritesOutput = {
-  MOVIE?: Favorite[];
-  ANIME?: Favorite[];
-};
+import {
+  DeleteFavoriteInput,
+  ErrorBody,
+  CreateFavoriteInput,
+  GetFavoritesOutput,
+  SessionUser,
+} from "@/lib/types";
 
 export async function GET(
   req: NextRequest
-): Promise<NextResponse<FavoritesOutput | ErrorBody>> {
+): Promise<NextResponse<GetFavoritesOutput | ErrorBody>> {
   const session = await getServerSession(authOptions);
 
   if (!session) {
@@ -38,7 +39,7 @@ export async function GET(
     favorites = await prisma.favorite.findMany();
   }
 
-  const output: FavoritesOutput = {};
+  const output: GetFavoritesOutput = {};
   for (const mType of mediaTypes) {
     output[mType] = favorites.filter((f) => f.mediaType === mType);
   }
@@ -46,12 +47,9 @@ export async function GET(
   return NextResponse.json(output);
 }
 
-type FavoriteInput = {
-  mediaType: MediaType;
-  mediaId: string;
-};
-
-export async function POST(req: NextRequest) {
+export async function POST(
+  req: NextRequest
+): Promise<NextResponse<Favorite | ErrorBody>> {
   const session = await getServerSession(authOptions);
 
   if (!session) {
@@ -64,7 +62,7 @@ export async function POST(req: NextRequest) {
   const user = session.user! as SessionUser;
   const userId = +user.id;
 
-  const favoriteInput = (await req.json()) as FavoriteInput;
+  const favoriteInput = (await req.json()) as CreateFavoriteInput;
 
   let newFavorite = null;
   try {
@@ -80,6 +78,54 @@ export async function POST(req: NextRequest) {
       },
     });
     return NextResponse.json(newFavorite, { status: 201 });
+  } catch (e: any) {
+    return NextResponse.json(
+      { error: true, message: e.message },
+      { status: 400 }
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest
+): Promise<NextResponse<Favorite | ErrorBody>> {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json(
+      { error: true, message: "Unauthorized" },
+      { status: 401 }
+    );
+  }
+
+  const user = session.user! as SessionUser;
+  const userId = +user.id;
+
+  const { id } = (await req.json()) as DeleteFavoriteInput;
+
+  const favorite = await prisma.favorite.findUnique({
+    where: { id },
+    select: { id: true, createdById: true },
+  });
+
+  if (!favorite) {
+    return NextResponse.json(
+      { error: true, message: "Not found" },
+      { status: 404 }
+    );
+  }
+
+  if (favorite.createdById !== userId) {
+    // Instead of sending Unauthorized error sending Not found error, so that hacker do not get any useful info.
+    return NextResponse.json(
+      { error: true, message: "Not found" },
+      { status: 404 }
+    );
+  }
+
+  try {
+    const deletedItem = await prisma.favorite.delete({ where: { id } });
+    return NextResponse.json(deletedItem, { status: 200 });
   } catch (e: any) {
     return NextResponse.json(
       { error: true, message: e.message },
--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -6,3 +6,17 @@ export type SessionUser = {
   email: string;
   image?: string | null;
 };
+
+type GetFavoritesOutput = {
+  MOVIE?: Favorite[];
+  ANIME?: Favorite[];
+};
+
+type CreateFavoriteInput = {
+  mediaType: MediaType;
+  mediaId: string;
+};
+
+type DeleteFavoriteInput = {
+  id: number;
+};
