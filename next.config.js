da480729b4cf45d7d3df98fab3f295fbcb735555
Mazedul Islam <mazidmailbox@gmail.com>
2023-08-29 10:50:38 +0600

    redirect home page to movies page

--- next.config.js
+++ next.config.js
@@ -14,6 +14,15 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/movies",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
