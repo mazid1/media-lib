29d010b97540d3baf8ab29740a6be8252cd72faa
Mazedul Islam <mazidmailbox@gmail.com>
2024-05-04 07:23:27 +0600

    revalidate movies page every hour

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -2,6 +2,8 @@ import React, { Suspense } from "react";
 import MovieList from "../components/movies/MovieList";
 import Loading from "./loading";
 
+export const revalidate = 60 * 60; // 1 hour
+
 function MoviesPage() {
   return (
     <Suspense fallback={<Loading />}>
